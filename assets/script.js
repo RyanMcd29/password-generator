759e9b189bd0c3170d01920620becbb4f01c592b
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2022-11-27 19:02:55 +0800

    Added loop to verify password length

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+function generatePassword() {
+
+// Variable Decleration
+
+var passwordLength = 1;
+var passwordCaps = false;
+var passwordNumbers = false;
+var passwordSpecial = false;
+
+// Set password length
+
+passwordLength <=8;
+passwordLength >128;
+
+
+do {
+  passwordLength = window.prompt ("What length should password be? (Pick between 8 to 128 characters" );
+  console.log (passwordLength);
+
+  console.log (passwordLength <8);
+  console.log (passwordLength >128);
+    if (passwordLength <8 || passwordLength >128) {
+      window.alert ("Password is an invalid length, please try again")
+    } else {
+      console.log ("Password length will be "+ passwordLength)
+    }
+} while (passwordLength <8 || passwordLength >128); //
+
+
+
+return;
+
+}
+
+//
+
+// Concat Arrays
+
+//
+ 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
