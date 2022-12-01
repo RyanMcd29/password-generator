d2fec3d2359872dc59bcf3f3db1a682ee63f3db1
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2022-12-01 23:15:19 +0800

    Removed commonly unsupported special characters

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ var password = [];
 var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "p", "w", "x", "y", "z"];
 var charactersCaps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "P", "W", "X", "Y", "Z"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialCharacters = ["!","?", "#", "$", "%", "&", "(", ")", "*", "+", "-", "<", ">", "=", "?", "@", "[", "]", "^", "_", "{", "}", "~" ];
+var specialCharacters = ["!","?", "#", "$", "%", "&", "(", ")", "?", "@", "[", "]", "^", "_", "{", "}", "~", "'", "/", "+", "-" ];
 
 // Set and verify password length
 //passwordLength <8
